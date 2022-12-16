b45604bc6dad49d743cd999c4234d281e7773ecd
Jinx <52132553+Dev-Jinx@users.noreply.github.com>
2022-12-16 09:23:45 +0000

    feat: discordImageURL

--- src/Website/Utils/index.js
+++ src/Website/Utils/index.js
@@ -2,5 +2,8 @@ class Utils {
   renderPage(req, res, page, data = {}) {
     res.render(page, data);
   }
+  discordImageURL(type, id, hash) {
+    return `https://cdn.discordapp.com/${type}/${id}/${hash}`
+  }
 }
 module.exports = new Utils;
