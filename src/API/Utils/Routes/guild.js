0a429145603744c26314cabebb584f4203efce9f
Jinx <52132553+Dev-Jinx@users.noreply.github.com>
2023-01-19 17:01:16 +0000

    feat: /api/guild endpoints

---
+++ src/API/Utils/Routes/guild.js
@@ -0,0 +1,104 @@
+const { Config } = require('../../../config');
+const { Logger } = require('../../../Logger');
+const Database = require('../database');
+const mysql = new Database(Config.API.DATABASE, Logger);
+const utils = require('../index');
+
+exports.create = async (req, res) => {
+  let api = req.headers['statify-api-key'];
+  if (!api) {
+    return res.json({ status: utils.HttpStatusCodes.BAD_REQUEST, message: 'No API Key provided' });
+  } else if (api !== Config.API.KEY) {
+    return res.json({ status: utils.HttpStatusCodes.FORBIDDEN, message: 'Invalid API Key provided' });
+  } else if (Object.keys(req.body).length !== 1) {
+    return res.json({ status: utils.HttpStatusCodes.BAD_REQUEST, message: 'No valid JSON body was found' });
+  }
+  const body = JSON.parse(JSON.stringify(req.body));
+  if (!mysql.connected) mysql.connect();
+  let query = undefined;
+  try {
+    query = await mysql.query(
+      'INSERT INTO guild (id) VALUES (?)',
+      body.discordID
+    );
+
+    res.json({ status: utils.HttpStatusCodes.CREATED, response: query });
+  } catch (error) {
+    Logger.RED('api', `GUILD CREATE MYSQL: ${error}`);
+    res.json({ status: utils.HttpStatusCodes.INTERNAL_SERVER_ERROR, error: error });
+  }
+}
+exports.get_guild = async (req, res) => {
+  let api = req.headers['statify-api-key'];
+  if (!api) {
+    return res.json({ status: utils.HttpStatusCodes.BAD_REQUEST, message: 'No API Key provided' });
+  } else if (api !== Config.API.KEY) {
+    return res.json({ status: utils.HttpStatusCodes.FORBIDDEN, message: 'Invalid API Key provided' });
+  } else if (Object.keys(req.body).length !== 1) {
+    return res.json({ status: utils.HttpStatusCodes.BAD_REQUEST, message: 'No valid JSON body was found' });
+  }
+  const body = JSON.parse(JSON.stringify(req.body));
+  if (!mysql.connected) mysql.connect();
+  let query = undefined;
+  try {
+    query = await mysql.query(
+      'SELECT * FROM guild WHERE id = ?',
+      body.discordID
+    );
+
+    res.json({ status: utils.HttpStatusCodes.OK, response: query });
+  } catch (error) {
+    Logger.RED('api', `GUILD GET MYSQL: ${error}`);
+    res.json({ status: utils.HttpStatusCodes.INTERNAL_SERVER_ERROR, error: error });
+  }
+}
+
+exports.increase_cmd_count = async (req, res) => {
+  let api = req.headers['statify-api-key'];
+  if (!api) {
+    return res.json({ status: utils.HttpStatusCodes.BAD_REQUEST, message: 'No API Key provided' });
+  } else if (api !== Config.API.KEY) {
+    return res.json({ status: utils.HttpStatusCodes.FORBIDDEN, message: 'Invalid API Key provided' });
+  } else if (Object.keys(req.body).length !== 1) {
+    return res.json({ status: utils.HttpStatusCodes.BAD_REQUEST, message: 'No valid JSON body was found' });
+  }
+  const body = JSON.parse(JSON.stringify(req.body));
+  if (!mysql.connected) mysql.connect();
+  let query = undefined;
+  try {
+    query = await mysql.query(
+      'UPDATE guild SET commands_count = commands_count + 1 WHERE id = ?',
+      body.discordID
+    );
+
+    res.json({ status: utils.HttpStatusCodes.OK, response: query });
+  } catch (error) {
+    Logger.RED('api', `GUILD CMD INCREASE MYSQL: ${error}`);
+    res.json({ status: utils.HttpStatusCodes.INTERNAL_SERVER_ERROR, error: error });
+  }
+}
+
+exports.delete = async (req, res) => {
+  let api = req.headers['statify-api-key'];
+  if (!api) {
+    return res.json({ status: utils.HttpStatusCodes.BAD_REQUEST, message: 'No API Key provided' });
+  } else if (api !== Config.API.KEY) {
+    return res.json({ status: utils.HttpStatusCodes.FORBIDDEN, message: 'Invalid API Key provided' });
+  } else if (Object.keys(req.body).length !== 1) {
+    return res.json({ status: utils.HttpStatusCodes.BAD_REQUEST, message: 'No valid JSON body was found' });
+  }
+  const body = JSON.parse(JSON.stringify(req.body));
+  if (!mysql.connected) mysql.connect();
+  let query = undefined;
+  try {
+    query = await mysql.query(
+      'DELETE FROM guild WHERE id = ?',
+      body.discordID
+    );
+
+    res.json({ status: utils.HttpStatusCodes.OK, response: query });
+  } catch (error) {
+    Logger.RED('api', `GUILD DELETE MYSQL: ${error}`);
+    res.json({ status: utils.HttpStatusCodes.INTERNAL_SERVER_ERROR, error: error });
+  }
+}
--- src/API/index.js
+++ src/API/index.js
@@ -2,18 +2,35 @@ const express = require('express');
 const { Config } = require('../config');
 const { Logger } = require('../Logger');
 const app = express();
+const { WebhookClient } = require('discord.js');
+const apiWebhook = new WebhookClient({ id: Config.WEBHOOKS.API_LOGS.ID, token: Config.WEBHOOKS.API_LOGS.TOKEN });
+const apiError = new WebhookClient({ id: Config.WEBHOOKS.API_ERRORS.ID, token: Config.WEBHOOKS.API_ERRORS.TOKEN });
+const sqlLog = new WebhookClient({ id: Config.WEBHOOKS.SQL_LOGS.ID, token: Config.WEBHOOKS.SQL_LOGS.TOKEN });
 const staff = require('./Utils/Routes/staff');
 const misc = require('./Utils/Routes/miscellaneous');
+const guild = require('./Utils/Routes/guild');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/api/staff', staff.get);
-app.delete('/api/staff/:discordID', staff.delete)
+app.delete('/api/staff/:discordID', staff.delete);
 app.post('/api/staff', staff.post);
+
+app.post('/api/guild/create', guild.create);
+app.get('/api/guild/get', guild.get_guild);
+app.post('/api/guild/cmd', guild.increase_cmd_count);
+app.delete('/api/guild/delete', guild.delete);
 
 app.post('/api/suggest', misc.postSuggest);
 
 app.listen(Config.API.PORT, () => {
   Logger.GREEN('API', 'Online');
+  if (!Config.DEVELOPMENT_MODE) apiWebhook.send({
+    embeds: [{
+      color: 0x417bd2,
+      author: { name: 'statify API' },
+      description: `API online using proxy port ${Config.API.PORT}\nTime: <t:${Math.floor(Date.now() / 1000)}:R>`
+    }]
+  });
 });
