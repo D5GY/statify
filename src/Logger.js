41529bed8a94925644bfb86ff47f2121f0308680
Jinx <52132553+Dev-Jinx@users.noreply.github.com>
2023-01-26 14:16:25 +0000

    style: removed space

--- src/Logger.js
+++ src/Logger.js
@@ -5,18 +5,18 @@ exports.Logger = {
     console.log(green(`[ statify ] [ Type: ${type.toLowerCase()} ] [ Date/Time: ${new Date().toLocaleString()} ] - ${string}`));
   },
   BLUE: (type, ...string) => {
-    console.log(blue(`[ statify ] [ Type: ${type.toLowerCase()} ] [  Date/Time: ${new Date().toLocaleString()} ] - ${string}`));
+    console.log(blue(`[ statify ] [ Type: ${type.toLowerCase()} ] [ Date/Time: ${new Date().toLocaleString()} ] - ${string}`));
   },
   RED: (type, ...string) => {
-    console.log(red(`[ statify ] [ Type: ${type.toLowerCase()} ] [  Date/Time: ${new Date().toLocaleString()} ] - ${string}`));
+    console.log(red(`[ statify ] [ Type: ${type.toLowerCase()} ] [ Date/Time: ${new Date().toLocaleString()} ] - ${string}`));
   },
   YELLOW: (type, ...string) => {
-    console.log(yellow(`[ statify ] [ Type: ${type.toLowerCase()} ] [  Date/Time: ${new Date().toLocaleString()} ] - ${string}`));
+    console.log(yellow(`[ statify ] [ Type: ${type.toLowerCase()} ] [ Date/Time: ${new Date().toLocaleString()} ] - ${string}`));
   },
   CYAN: (type, ...string) => {
-    console.log(cyan(`[ statify ] [ Type: ${type.toLowerCase()} ] [  Date/Time: ${new Date().toLocaleString()} ] - ${string}`));
+    console.log(cyan(`[ statify ] [ Type: ${type.toLowerCase()} ] [ Date/Time: ${new Date().toLocaleString()} ] - ${string}`));
   },
   MAGENTA: (type, ...string) => {
-    console.log(magenta(`[ statify ] [ Type: ${type.toLowerCase()} ] [  Date/Time: ${new Date().toLocaleString()} ] - ${string}`));
+    console.log(magenta(`[ statify ] [ Type: ${type.toLowerCase()} ] [ Date/Time: ${new Date().toLocaleString()} ] - ${string}`));
   }
 }
