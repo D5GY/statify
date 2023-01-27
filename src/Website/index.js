8b947aa6958f23cf0f20aa1721ecf5b136fd6873
Jinx <52132553+Dev-Jinx@users.noreply.github.com>
2023-01-27 16:54:12 +0000

    fix: routes and webhook

--- src/Website/index.js
+++ src/Website/index.js
@@ -1,7 +1,7 @@
 const getFiles = new Date();
 const { WebhookClient } = require('discord.js');
 const { Logger } = require('../Logger');
-const { WEBSITE, DEVELOPMENT_MODE } = require('../config').Config;
+const { WEBSITE, DEVELOPMENT_MODE, WEBHOOKS } = require('../config').Config;
 const { join } = require('path');
 const express = require('express');
 const app = express();
@@ -9,7 +9,7 @@ const ejs = require('ejs');
 const passport = require('passport');
 const session = require('express-session');
 const crypto = require('crypto');
-const WebsiteOnline = new WebhookClient({ id: WEBSITE.WEBHOOKS.ONLINE.ID, token: WEBSITE.WEBHOOKS.ONLINE.TOKEN });
+const WebsiteOnline = new WebhookClient({ id: WEBHOOKS.WEBSITE_LOGS.ID, token: WEBHOOKS.WEBSITE_LOGS.TOKEN });
 
 app.use('html', ejs.renderFile);
 app.set('views', join(__dirname, 'Pages'));
@@ -21,21 +21,22 @@ app.use(session({ secret: crypto.randomBytes(20).toString('hex'), resave: true, 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/api/login', require('./Utils/Auth/Login'));
+app.get('/auth/login', require('./Utils/Auth/Login'));
 app.get('/logout', require('./Utils/Auth/Logout'));
+app.get('/login', require('./Utils/Routes/login'));
 
 app.get('/', require('./Utils/Routes/home'));
 app.get('/commands', require('./Utils/Routes/commands'));
 app.get('/status', require('./Utils/Routes/status'));
 app.get('/about', require('./Utils/Routes/about'));
 
-app.get('/login', require('./Utils/Routes/login'));
-app.get('/servers', require('./Utils/Routes/servers'));
-app.get('/dashboard', require('./Utils/Routes/dashboard'));
-
-app.get('/dashboard/information', require('./Utils/Routes/information'));
-app.get('/dashboard/customize', require('./Utils/Routes/customize'));
-app.get('/dashboard/settings', require('./Utils/Routes/settings'));
+app.get('/dashboard', require('./Utils/Routes/Dashboard/index'));
+app.get('/dashboard/:id/information', require('./Utils/Routes/Dashboard/information'));
+app.get('/dashboard/:id/customize', require('./Utils/Routes/Dashboard/customize'));
+app.get('/dashboard/:id/settings', require('./Utils/Routes/Dashboard/settings'));
+app.get('/dashboard/:id/remove', (req, res) => {
+  
+});
 
 app.get('/discord', (req, res) => { res.redirect(WEBSITE.URLs.DISCORD_INVITE) });
 app.get('/invite', (req, res) => { res.redirect(WEBSITE.URLs.BOT_INVITE) });
@@ -53,7 +54,7 @@ app.listen(WEBSITE.PORT, () => {
   Logger.BLUE('website', `Online on port ${WEBSITE.PORT}`);
   if (!DEVELOPMENT_MODE) {
     WebsiteOnline.send({
-      embeds: [{ color: '', author: { name: 'statify' }, description: 'Website is now online using proxy port 3000', timestamp: new Date().toLocaleString() }]
+      embeds: [{ color: '', author: { name: 'statify' }, description: `Website is now online using proxy port ${WEBSITE.PORT}`, timestamp: new Date().toLocaleString() }]
     }).catch((error) => { Logger.RED('website', `Seems a problem was encountered sending website online webhook. \n Error: ${error}`) });
   }
 });
