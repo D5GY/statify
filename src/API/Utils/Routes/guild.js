ed69e582d8af69c15d7c711e47d35d5118c3dc35
Jinx <52132553+Dev-Jinx@users.noreply.github.com>
2023-01-28 22:38:11 +0000

    feat(api): get all guilds

--- src/API/Utils/Routes/guild.js
+++ src/API/Utils/Routes/guild.js
@@ -45,7 +45,30 @@ exports.get_guild = async (req, res) => {
       'SELECT * FROM guild WHERE id = ?',
       body.discordID
     );
+    res.json({ status: utils.HttpStatusCodes.OK, response: query });
+  } catch (error) {
+    Logger.RED('api', `GUILD GET MYSQL: ${error}`);
+    res.json({ status: utils.HttpStatusCodes.INTERNAL_SERVER_ERROR, error: error });
+  }
+}
 
+exports.get_all = async (req, res) => {
+  let api = req.headers['statify-api-key'];
+  if (!api) {
+    return res.json({ status: utils.HttpStatusCodes.FORBIDDEN, message: 'No API Key provided' });
+  } else if (api !== Config.API.KEY) {
+    return res.json({ status: utils.HttpStatusCodes.FORBIDDEN, message: 'Invalid API Key provided' });
+  } else if (Object.keys(req.body).length !== 1) {
+    return res.json({ status: utils.HttpStatusCodes.BAD_REQUEST, message: 'No valid JSON body was found' });
+  }
+  const body = JSON.parse(JSON.stringify(req.body));
+  if (!mysql.connected) mysql.connect();
+  let query = undefined;
+  try {
+    query = await mysql.query(
+      'SELECT * FROM guild',
+      body.discordID
+    );
     res.json({ status: utils.HttpStatusCodes.OK, response: query });
   } catch (error) {
     Logger.RED('api', `GUILD GET MYSQL: ${error}`);
