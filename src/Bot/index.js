8a0712a5c6a5a0c0117c526f9370bded6d0be314
Jinx <52132553+Dev-Jinx@users.noreply.github.com>
2023-02-21 10:35:02 +0000

    feat: error events

--- src/Bot/index.js
+++ src/Bot/index.js
@@ -1,1 +1,57 @@
-require('./Core/statify').start();
+const statify = require('./Core/statify');
+statify.start();
+
+// Log all errors and warnings.
+
+statify.on('error', (error) => {
+  statify.logger.RED(bot, error);
+  statify.webhooks.errorLogs.send({
+    content: '<@&1037116954416255007>',
+    embeds: [{
+      color: statify.Colors.RED,
+      author: { name: `statify client error: ${error.name}` },
+      description: error.message
+    }]
+  });
+});
+
+statify.on('warn', (message) => {
+  statify.logger.YELLOW('bot', message);
+  statify.webhooks.errorLogs.send({
+    content: '<@&1037116954416255007>',
+    embeds: [{
+      color: statify.Colors.YELLOW,
+      author: { name: `statify client warning` },
+      description: message
+    }]
+  });
+});
+
+if (statify.config.DEVELOPMENT_MODE) {
+  statify.on('debug', (message) => {
+    statify.logger.YELLOW('bot', message)
+  });
+}
+
+process.on('uncaughtException', (error) => {
+  statify.logger.RED(bot, error);
+  statify.webhooks.errorLogs.send({
+    content: '<@&1037116954416255007>',
+    embeds: [{
+      color: statify.Colors.RED,
+      author: { name: `statify uncaughtException: ${error.name}` },
+      description: error.message
+    }]
+  });
+});
+
+process.on('unhandledRejection', (error) => {
+  statify.logger.RED(bot, error);
+  statify.webhooks.errorLogs.send({
+    content: '<@&1037116954416255007>',
+    embeds: [{
+      color: statify.Colors.RED,
+      description: 'statify unhandledRejection, check VPS logs!'
+    }]
+  });
+});
